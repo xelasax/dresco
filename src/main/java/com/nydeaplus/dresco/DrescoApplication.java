dcb5682a6eeee05e9cdcea6b4fb4501d83451f95
Quame <quamelexiz@gmail.com>
2019-01-27 14:11:11 +0000

    first commit

---
+++ src/main/java/com/nydeaplus/dresco/DrescoApplication.java
@@ -0,0 +1,14 @@
+package com.nydeaplus.dresco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrescoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrescoApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/nydeaplus/dresco/DrescoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nydeaplus.dresco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DrescoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
