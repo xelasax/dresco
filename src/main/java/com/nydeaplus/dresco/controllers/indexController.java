0e6a07f2774fde948f4b98ccb6c94f5c187741b8
Quame <quamelexiz@gmail.com>
2019-01-27 14:26:55 +0000

    Index Controller and Placeholder Created

---
+++ src/main/java/com/nydeaplus/dresco/controllers/indexController.java
@@ -0,0 +1,14 @@
+package com.nydeaplus.dresco.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping("/")
+    public String index(){
+
+        return "index";
+    }
+}
